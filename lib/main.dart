01a0d8c8ee257440d6704c3996fca9105f969cbd
Mahfjsggs <mhmedtrej@gmail.com>
2026-07-30 05:07:38 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import 'app/app.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await MobileAds.instance.initialize();
+
+  runApp(const RewardApp());
+}
