ff76e39f7d6609f22a16f71fc8a088ab870e3df0
Mahfjsggs <mhmedtrej@gmail.com>
2026-08-01 18:53:03 +0300

    Update app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../features/rewards/rewards_page.dart';
 import '../features/wallet/wallet_page.dart';
+import '../features/withdrawals_feed/withdrawals_page.dart';
 import '../screens/auth/login_page.dart';
 import '../screens/auth/register_page.dart';
 import '../screens/home/home_page.dart';
@@ -63,6 +64,7 @@ class RewardApp extends StatelessWidget {
         '/home': (context) => const HomePage(),
         '/rewards': (context) => const RewardsPage(),
         '/wallet': (context) => const WalletPage(),
+        '/withdrawals': (context) => const WithdrawalsFeedPage(),
       },
     );
   }
