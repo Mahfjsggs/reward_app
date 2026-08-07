af144ed9d1a9d9db1eac12a04da0541af5185966
Mahfjsggs <mhmedtrej@gmail.com>
2026-08-07 16:06:53 +0300

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,12 @@ import * as admin from "firebase-admin";
 
 admin.initializeApp();
 
+// الوظائف الموجودة سابقاً
 export { startAdSession, grantAdReward } from "./rewards";
 export { requestWithdrawal, onWithdrawalCompleted } from "./withdrawals";
 export { updateWeeklyLeaderboard } from "./leaderboard";
 export { spinWheel } from "./spinWheel";
+
+// الوظائف الجديدة (المكافأة اليومية والإحالة والعروض)
+export { claimDailyBonus, applyReferralCode } from "./dailyBonus";
+export { offerwallPostback } from "./offerwalls";
